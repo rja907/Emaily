b4ff28c3da9a81fe6b6ce6edfcd17d2b700cca67
Raj <kmrrajj@gmial.com>
2017-11-05 21:52:45 +0530

    Adding Billing and Client Side App

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express'); //Common JS module
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(bodyParser.json());
 //app.use : middleware used to modify incoming req before they sent off to different route handlers
 app.use(
   cookieSession({
@@ -17,5 +19,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  //express serves prod assets like main.js and main.css
+  app.use(express.static('client/build'));
+  //express serves index.html if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000; //environment variables
 app.listen(PORT); //5000 = port number
--- services/passport.js
+++ services/passport.js
@@ -19,17 +19,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-          //returning member
-        } else {
-          new User({ googleId: profile.id }) //model instance
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+        //returning member
+      }
+      const user = await new User({ googleId: profile.id }).save(); //model instance
+      done(null, user);
     }
   )
 );
