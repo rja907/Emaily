7fae7b2fe675db6d2276e337428e86c7195f16e5
Raj <kmrrajj@gmial.com>
2017-11-06 21:22:46 +0530

    Users can see Survey Results

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   //class based component
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
@@ -20,7 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-
+require('./routes/surveyRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   //express serves prod assets like main.js and main.css
   app.use(express.static('client/build'));
